65d5b98cbbbb5e57a1b59a765b708bf6be554827
Jai Marothiya <jaimarothiya48@gmail.com>
2022-12-24 09:37:45 +0530

    add display style in input section, so when we click on input it show result.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ class App extends React.Component {
       items: this.props.data.slice(0,8),
       input:"",
       display:"none",
+      search:[],
     };
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleOnChange(e){
@@ -20,34 +22,49 @@ class App extends React.Component {
     let links = [];
     let count=0;
     let reg = new RegExp(value);
-    for(let j=0;j<data.length;j++){
-      if(reg.test(data[j])){
-        links[count]=data[j];
+    this.setState({
+      search:e.target,
+    });
+
+    data.forEach((ele) => {
+      if(reg.test(ele) && count<8){
+        links[count]=ele;
         count++;
       } 
-
-      if(count>7){
-        break;
-      }
-    }
-
+    });
     this.setState({
       input: value,
       items: [...links],
     })
-}
+  }
 
+  handleClick(e){
+
+    
+
+  }
   
 
   
   render(){
+    window.addEventListener("click",(event)=>{
+        if(event.target===this.state.search){
+          this.setState({
+          display:block,
+          });
+        }else{
+          this.setState({
+            display:none,
+          });
+        }
+    });
     
     return (
       <div className="App">
         <header>Get Set GO</header>
         <div>
           <span>
-            <input type="text" name="" id="" placeholder="Type a URL...." value={this.state.input} onChange={this.handleOnChange}/>
+            <input type="text" name="" id="" placeholder="Type a URL...." value={this.state.input} onChange={this.handleOnChange} onClick={this.handleClick}/>
           </span>
           <Search items={this.state.items} display={this.state.display}></Search>
         </div>
